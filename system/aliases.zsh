0dc490c17c32be14c4a77b623a705f9cc6f09eee
devlin zed <me@devlinzed.com>
2013-06-10 11:53:46 -0400

    one-letter aliases

--- system/aliases.zsh
+++ system/aliases.zsh
@@ -11,6 +11,14 @@ for scr in rake; do
   alias "$scr"="noglob $scr";
 done
 
+for scr in git vim workon; do
+  alias "${scr:0:1}"="$scr"
+done
+
+if command -v hub &> /dev/null; then
+  eval "$(hub alias -s)"
+fi
+
 alias "1st"="awk '{ print \$1 }'"
 alias "2nd"="awk '{ print \$2 }'"
 alias "3rd"="awk '{ print \$3 }'"
@@ -18,10 +26,3 @@ alias "3rd"="awk '{ print \$3 }'"
 for i in $(seq 4 9); do
   alias "${i}th"="awk '{ print \$$i }'"
 done
-
-if command -v hub &> /dev/null; then
-  eval "$(hub alias -s)"
-fi
-
-alias "fucking"="sudo"
-alias "please"="sudo"
