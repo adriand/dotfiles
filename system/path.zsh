058436ead775c9a8019a939fe7801784df5f790e
devlin zed <me@devlinzed.com>
2013-06-14 09:34:38 -0400

    zsh tweaks

--- system/path.zsh
+++
@@ -1,5 +0,0 @@
-PATH="./.bundle/binstubs:$DOTFILES/bin:$HOME/.local/bin:/usr/local/bin:/usr/bin:/bin:/usr/local/games:/usr/games"
-
-[[ -d "$HOME/.rbenv" ]] && PATH="$HOME/.rbenv/shims:$HOME/.rbenv/bin:$PATH"
-
-export PATH
